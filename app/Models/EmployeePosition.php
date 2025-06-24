c2241965dfd7ee05072fabb8717c38530b57bd8d
Rizky Dhani <rizkydhani15@gmail.com>
2025-06-24 13:27:59 +0700

    Add EmployeePosition model and migration for employee_positions table

---
+++ app/Models/EmployeePosition.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmployeePosition extends Model
+{
+    protected $guarded = ['id'];
+
+    public function getRouteKeyName(): string
+    {
+        return 'positionId';
+    }
+
+    public function division()
+    {
+        return $this->belongsTo(EmployeeDivision::class, 'division_id');
+    }
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class, 'position_id');
+    }
+}
---
+++ database/migrations/2025_06_24_045122_create_employee_positions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employee_positions', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('positionId')->unique();
+            $table->foreignId('division_id')->constrained('employee_divisions')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employee_positions');
+    }
+};
