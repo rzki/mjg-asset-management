a97b86091f82838b1b5d57f7028398192cc62e40
Rizky Dhani <rizkydhani15@gmail.com>
2025-06-25 11:05:14 +0700

    Add DivisionSeeder to seed employee divisions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
+            DivisionSeeder::class,
+            PositionSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/DivisionSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use App\Models\EmployeeDivision;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class DivisionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        EmployeeDivision::create([
+            'divisionId' => Str::orderedUuid(),
+            'name' => 'Information & Technology',
+        ]);
+    }
+}
