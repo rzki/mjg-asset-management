0536bb7e5ec49189e242a820635208f40a33a997
Rizky Dhani <rizkydhani15@gmail.com>
2025-06-25 09:31:35 +0700

    Milon/barcode package installation; Add barcode generation for assets; update CreateAsset page and composer dependencies

--- app/Filament/Resources/AssetResource/Pages/CreateAsset.php
+++ app/Filament/Resources/AssetResource/Pages/CreateAsset.php
@@ -2,8 +2,10 @@
 
 namespace App\Filament\Resources\AssetResource\Pages;
 
-use App\Filament\Resources\AssetResource;
 use Filament\Actions;
+use Milon\Barcode\DNS2D;
+use Illuminate\Support\Facades\Storage;
+use App\Filament\Resources\AssetResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateAsset extends CreateRecord
@@ -13,6 +15,12 @@ class CreateAsset extends CreateRecord
     {
         $data['assetId'] = \Illuminate\Support\Str::orderedUuid();
         $data['pic_id'] = auth()->user()->id; // Automatically set the current user as the PIC
+        $route = route('assets.show', ['assetId' => $data['assetId']]);
+        $qr = new DNS2D();
+        $qrCodeImage = base64_decode($qr->getBarcodePNG($route, 'QRCODE'));
+        $path = 'assets/' . $data['assetId'].'.png';
+        $data['barcode'] = $path;
+        Storage::disk('public')->put($path, $qrCodeImage);
         return $data;
     }
     protected function getRedirectUrl(): string
