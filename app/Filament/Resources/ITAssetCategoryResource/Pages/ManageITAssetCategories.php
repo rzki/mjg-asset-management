19b96e7445869f949aa074ecc559de9cd6938feb
Rizky Dhani <rizkydhani15@gmail.com>
2025-06-26 10:11:54 +0700

    Add label to Create Category action in ManageITAssetCategories page

--- app/Filament/Resources/ITAssetCategoryResource/Pages/ManageITAssetCategories.php
+++ app/Filament/Resources/ITAssetCategoryResource/Pages/ManageITAssetCategories.php
@@ -13,7 +13,8 @@ class ManageITAssetCategories extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label('Create Category'),
         ];
     }
 }
