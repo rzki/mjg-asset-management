1c81b9814b2e2760934511ad01014df032ddfa89
Rizky Dhani <rizkydhani15@gmail.com>
2025-06-30 17:43:01 +0700

    Add labels to create actions for Employee Divisions, Positions, Users, and Employees for improved clarity

--- app/Filament/Resources/EmployeeDivisionResource/Pages/ListEmployeeDivisions.php
+++ app/Filament/Resources/EmployeeDivisionResource/Pages/ListEmployeeDivisions.php
@@ -13,7 +13,8 @@ class ListEmployeeDivisions extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+            ->label('New Employee Division'),
         ];
     }
 }
--- app/Filament/Resources/EmployeeResource/Pages/ManageEmployees.php
+++ app/Filament/Resources/EmployeeResource/Pages/ManageEmployees.php
@@ -13,7 +13,8 @@ class ManageEmployees extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label('New Employee'),
         ];
     }
 }
