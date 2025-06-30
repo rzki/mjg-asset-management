969809d2610a0907c489a16968ff5e933fbbbada
Rizky Dhani <rizkydhani15@gmail.com>
2025-06-30 16:46:06 +0700

    Add getRouteKeyName method to ITAssetCategory model to use 'code' as route key

--- app/Models/ITAssetCategory.php
+++ app/Models/ITAssetCategory.php
@@ -8,6 +8,10 @@ class ITAssetCategory extends Model
 {
     protected $guarded = ['id'];
     protected $table = 'it_asset_categories';
+    public function getRouteKeyName()
+    {
+        return 'code';
+    }
     public function assets()
     {
         return $this->hasMany(ITAsset::class, 'asset_category_id', 'name');
