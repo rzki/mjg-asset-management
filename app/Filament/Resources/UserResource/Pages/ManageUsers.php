31c3fcdc68d7ad7fffb2d576de780a19dc57b6c2
Rizky Dhani <rizkydhani15@gmail.com>
2025-06-30 16:46:34 +0700

    Create ManageUsers page to handle user management actions

--- app/Filament/Resources/UserResource/Pages/ManageUsers.php
+++ app/Filament/Resources/UserResource/Pages/ManageUsers.php
@@ -2,11 +2,11 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
-use App\Filament\Resources\UserResource;
 use Filament\Actions;
-use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\UserResource;
+use Filament\Resources\Pages\ManageRecords;
 
-class ListUsers extends ListRecords
+class ManageUsers extends ManageRecords
 {
     protected static string $resource = UserResource::class;
 
